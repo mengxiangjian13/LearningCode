298a66e5de879b81eee2bec8a96a6610914ea952
Xiangjian Meng <mengxiangjian13@163.com>
2014-04-29 11:22:41 +0800

    learn facebook pop

---
+++ FacebookPopTest/FacebookPopTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FacebookPopTest
+//
+//  Created by Xiangjian Meng on 14-4-29.
+//  Copyright (c) 2014年 cn.com.modernmedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
