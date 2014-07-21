cee13ae035e43681ed90f3bbc04e668ed5e6978d
Xiangjian Meng <mengxiangjian13@163.com>
2014-07-21 17:46:12 +0800

    ARC

---
+++ playground/ARC.playground/section-1.swift
@@ -0,0 +1,79 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+// weak reference
+
+class Person {
+    let name : String
+    init(name:String)
+    {
+        self.name = name
+    }
+    var apartment : Apartment?
+    
+    deinit{
+        let a = "ggggg"
+    }
+}
+
+class Apartment
+{
+    let number : Int
+    init (number : Int)
+    {
+        self.number = number
+    }
+    
+    // must weak
+    weak var tenant : Person?
+    
+    deinit{
+        let a = "ggggg"
+    }
+}
+
+var john : Person?
+var number73 : Apartment?
+
+john = Person(name: "john")
+number73 = Apartment(number: 73)
+
+john!.apartment = number73
+number73!.tenant = john
+
+john = nil
+
+
+// unowned reference
+
+class Customer {
+    let name : String
+    var card : CreditCard?
+    init(name:String)
+    {
+        self.name = name
+    }
+}
+
+class CreditCard {
+    let number : Int
+    unowned let customer : Customer
+    init(number:Int, customer:Customer)
+    {
+        self.number = number
+        self.customer = customer
+    }
+}
+
+var meng : Customer?
+meng = Customer(name: "meng")
+meng!.card = CreditCard(number: 123333, customer: meng!)
+
+
+
+
+
+
