67e4d695407bb5028a1512d040d2f4825eadc544
Xiangjian Meng <mengxiangjian13@163.com>
2014-08-18 18:35:56 +0800

    AdaptiveLayout

---
+++ iOS8/AdaptiveLayout/AdaptiveLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveLayout
+//
+//  Created by Xiangjian Meng on 14-8-18.
+//  Copyright (c) 2014年 cn.com.modernmedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
