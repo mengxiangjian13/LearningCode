3da4e7642a1aa7d063992d4680c7b1cb1cb22266
Xiangjian Meng <mengxiangjian13@163.com>
2014-09-01 18:52:03 +0800

    GLKit

---
+++ GLKitLearning/GLKitLearning/ColorfulSquareViewController.h
@@ -0,0 +1,15 @@
+//
+//  ColorfulSquareViewController.h
+//  GLKitLearning
+//
+//  Created by Xiangjian Meng on 14-9-1.
+//  Copyright (c) 2014年 cn.com.modernmedia. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface ColorfulSquareViewController : GLKViewController
+
+@property (nonatomic,strong) EAGLContext *context;
+
+@end
---
+++ GLKitLearning/GLKitLearning/ColorfulSquareViewController.m
@@ -0,0 +1,91 @@
+//
+//  ColorfulSquareViewController.m
+//  GLKitLearning
+//
+//  Created by Xiangjian Meng on 14-9-1.
+//  Copyright (c) 2014年 cn.com.modernmedia. All rights reserved.
+//
+
+#import "ColorfulSquareViewController.h"
+
+typedef struct{
+    float Position[3];
+    float Color[4];
+}Vertex;
+
+const Vertex Vertices[] = {
+    {{1,-1,0},{1,0,0,1}},
+    {{1, 1, 0}, {0, 1, 0, 1}},
+    {{-1, 1, 0}, {0, 0, 1, 1}},
+    {{-1, -1, 0}, {0, 0, 0, 1}}
+};
+
+const GLubyte Indices[] = {
+    0, 1, 2,
+    2, 3, 0
+};
+
+@interface ColorfulSquareViewController ()
+{
+    GLuint _vertexBuffer;
+    GLuint _indexBuffer;
+}
+
+@property (nonatomic,strong) GLKBaseEffect *effect;
+
+@end
+
+@implementation ColorfulSquareViewController
+@synthesize effect = _effect;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self setupGL];
+    
+}
+
+- (void)setupGL
+{
+    [EAGLContext setCurrentContext:self.context];
+    
+    self.effect = [[GLKBaseEffect alloc] init];
+    
+    glGenBuffers(1, &_vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    
+    glGenBuffers(1, &_indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
