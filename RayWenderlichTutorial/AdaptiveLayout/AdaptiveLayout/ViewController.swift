f4a0309bd617a6ea6b301106ece751c26c8d1966
Xiangjian Meng <mengxiangjian13@163.com>
2014-10-13 15:25:45 +0800

    Adaptive Layout from raywenderlich

---
+++ RayWenderlichTutorial/AdaptiveLayout/AdaptiveLayout/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AdaptiveLayout
+//
+//  Created by Xiangjian Meng on 14-10-13.
+//  Copyright (c) 2014年 cn.com.modernmedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor(red: (74.0 / 255.0), green: (171.0 / 255.0), blue: (247.0 / 255.0), alpha: 1.0);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
