23480c83afac3d06626097f5359bc4cb5b2aec0a
Xiangjian Meng <mengxiangjian13@163.com>
2014-12-08 18:26:25 +0800

    alamofire custom serialize

--- AlamofireDemo/AlamofireDemo/ViewController.swift
+++ AlamofireDemo/AlamofireDemo/ViewController.swift
@@ -9,6 +9,48 @@
 import UIKit
 import Alamofire
 
+class WeatherObject{
+    var temperature : NSNumber
+    init(temperature:NSNumber){
+        self.temperature = temperature
+    }
+}
+
+// 自定义Serializer。将NSData改换成Weather对象
+extension Alamofire.Request{
+    class func objectResponseSerializer() -> Serializer {
+        return { request, response, data in
+            if data == nil {
+                return (nil,nil)
+            }
+            
+            var error : NSErrorPointer = nil
+            
+            let dict: AnyObject? =  NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments, error: error)
+            
+            if let d = dict as? NSDictionary {
+                let main : AnyObject? = d["main"]
+                if let t = main as? NSDictionary {
+                    let temperature: AnyObject? = t["temp"]
+                    if let temp = temperature as? NSNumber {
+                        let weather = WeatherObject(temperature: temp)
+                        return (weather,nil)
+                    }
+                }
+            }
+            
+            return (nil,error.memory)
+        }
+    }
+    
+    func responseWeather(completionHandler: (NSURLRequest, NSHTTPURLResponse?, WeatherObject?, NSError?) -> Void) -> Self {
+        return response(serializer: Request.objectResponseSerializer()){
+            request,response,data,error in
+            completionHandler(request,response,data as? WeatherObject,error)
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -19,6 +61,10 @@ class ViewController: UIViewController {
             (_,_,data,_) in println(data)
         }
         
+        Alamofire.request(.GET, "http://api.openweathermap.org/data/2.5/weather?lat=40&lon=116&units=imperial").responseWeather() {
+            (_,_,data,_) in println("temp:\(data?.temperature)")
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
