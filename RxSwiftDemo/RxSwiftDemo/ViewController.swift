01a1894c3bcbba29168c0ea5259b9fbbcd4c8732
孟祥建 <mengxiangjian@letv.com>
2016-01-14 14:19:21 +0800

    bugfix

--- RxSwiftDemo/RxSwiftDemo/ViewController.swift
+++ RxSwiftDemo/RxSwiftDemo/ViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 import RxSwift
+//import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var a = [1,2,3,4,5,6]
+        a.append(7)
+        a.toObservable().subscribeNext {
+            x in
+            print(x)
+        }
+        
         
     }
 
