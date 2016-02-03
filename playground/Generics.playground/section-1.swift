748a1079500d59fe6c85ac7aa5f8507596715276
孟祥建 <mengxiangjian@letv.com>
2016-02-03 18:06:00 +0800

    Swift Generic & which

--- playground/Generics.playground/section-1.swift
+++ playground/Generics.playground/section-1.swift
@@ -8,7 +8,7 @@ var str = "Hello, playground"
 
 //<>里面的T是泛型自己定义的类型，告诉swift不去追求T到底是什么类型。等真正被调用的时候，传进来的参数是什么类型，T就会被那个类型替换
 // 不是必须使用T来表示泛型类型，可以使用其他名称，但要保持大写字母开头的驼峰式命名
-func swapTwoItems<T>(inout a:T,inout b:T)
+func swapTwoItems<T>(inout a:T,inout _ b:T)
 {
     let temp = a
     a = b
@@ -17,7 +17,7 @@ func swapTwoItems<T>(inout a:T,inout b:T)
 
 var a = 4
 var b = 5
-swap(&a, &b)
+swapTwoItems(&a, &b)
 a
 b
 
@@ -53,10 +53,37 @@ func findIndex<T:Equatable>(array:[T],valueToFind:T) -> Int?
 findIndex([1,2,3], valueToFind: 2)
 
 // 关联类型行为
+protocol SomeProtocol {
+    typealias ItemType
+    var a : ItemType {get set}
+}
 
-// where 语句
+struct IntStruct : SomeProtocol {
+    var a : Int = 3
+}
+
+struct StringStruct : SomeProtocol {
+    var a : String = "3"
+}
 
 
+// where 语句 给泛型类型以控制
+func allItemMatch<T1:SomeProtocol,T2:SomeProtocol where T1.ItemType == T2.ItemType, T1.ItemType:Equatable>(a:T1,b:T2) -> Bool {
+    if a.a == b.a {
+        return true
+    }
+    return false
+}
 
+let someInt = IntStruct(a: 6)
+let someInt2 = IntStruct(a: 3)
+let someInt3 = IntStruct()
+let someString = StringStruct(a: "4")
+let someString2 = StringStruct(a: "4")
 
+//let match = allItemMatch(someString, b: someInt2) // 不符合1 2类型相同
+
+let match = allItemMatch(someInt, b: someInt2)
+let match1 = allItemMatch(someInt2, b: someInt3)
+let match2 = allItemMatch(someString, b: someString2)
 
